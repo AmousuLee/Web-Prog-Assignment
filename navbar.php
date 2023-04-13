aa671c2d9b1fa5033cee28e0598f5ee14ceef7c8
[redacted] <r3tr0_02@protonmail.com>
2023-04-14 04:01:43 +0800

    Update navbar.php
    - Add `if...elseif...else` to compl. admin, user and visitor navbar

--- navbar.php
+++ navbar.php
@@ -9,14 +9,28 @@
         <div class="container px-5">
             <a class="navbar-brand" href="#page-top">Archery Tournament</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="navbar-toggler-icon"></span></button>
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
                     <?php
-                        if(isset($_SESSION['username'])){
-                            echo '<li class="nav-item"><a class="nav-link" href="#!">Welcome '; echo $_SESSION['username']; echo '</a></li>';
-                        }else{
+                        // if logged in as admin
+                        if(isset($_SESSION['username']) == "admin")
+                        {
+                            echo '<li class="nav-item"><a class="nav-link" href="#!">Record</a></li>';
+                            echo '<li class="nav-item"><a class="nav-link" href="#!">Logout</a></li>';
+                        }
+                        // if logged in as user
+                        else if (isset($_SESSION['username']))
+                        {
+                            echo '<li class="nav-item"><a class="nav-link" href="#!">Register</a></li>';
+                            echo '<li class="nav-item"><a class="nav-link" href="#!">Profile</a></li>';
+                            echo '<li class="nav-item"><a class="nav-link" href="#!">Logout</a></li>';
+                        }
+                        // else sess. var not init : not logged in
+                        else
+                        {
                             echo '<li class="nav-item"><a class="nav-link" href="#!">Sign Up</a></li>';
                             echo '<li class="nav-item"><a class="nav-link" href="#!">Log In</a></li>';
                         }
