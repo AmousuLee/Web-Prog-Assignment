dd843c31abf71be0d565aaf4b75282c0c8c9f9b9
Atzovka <andrewhiu02@gmail.com>
2023-04-14 20:26:56 +0800

    nigga

--- register.php
+++ register.php
@@ -16,26 +16,18 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i"
         rel="stylesheet" />
     <!-- registration  -->
-    <link href="regForm.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="assets/css/styles.css" rel="stylesheet" />
+    <link href="assets/css/regForm.css" rel="stylesheet" />
 </head>
 
+
+
 <body id="page-top">
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-        <div class="container px-5">
-            <a class="navbar-brand" href="#page-top">Archery Tournament</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="#!">Sign Up</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">Log In</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+
+
+    <?php
+        include("assets/navbar.php");
+    ?>
 
 
     <!-- Header-->
@@ -107,11 +99,9 @@
 
 
     <!-- Footer-->
-    <footer class="py-5 bg-black">
-        <div class="container px-5">
-            <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2023</p>
-        </div>
-    </footer>
+    <?php
+        include("assets/footer.html");
+    ?>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
