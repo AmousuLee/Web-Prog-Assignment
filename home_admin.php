b513a1e3807e218320504edb0819e84d2e0ad19b
[redacted] <r3tr0_02@protonmail.com>
2023-04-17 02:49:44 +0800

    Add profile_user, updateProfile
    - Regex check to change navbar to corresp. page (registerEvent/profile_user)
    - Also add forgotten js at bottom of files

--- home_admin.php
+++ home_admin.php
@@ -46,5 +46,11 @@
         <?php
             include("assets/footer.html");
         ?>
+
+        <!-- Bootstrap core JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS -->
+        <script src="assets/js/scripts.js"></script>
+        <script src="assets/js/validation.js"></script>
     </body>
 </html>
