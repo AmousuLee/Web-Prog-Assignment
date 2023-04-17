6719b344d2c0772da414ecf9865cc8120bf85734
AmousuLee <leeamous@gmail.com>
2023-04-17 13:29:31 +0800

    Database Connection to External
    Clean Some Code

--- assets/registerEventProcess.php
+++ assets/registerEventProcess.php
@@ -1,55 +1,54 @@
-<?php session_start(); ?>
-<html>
-    <head>
-    </head>
-    <body>
-        <?php
-            $category = $_POST["category"];
-            $name = $_SESSION["name"];
+<script>
+    function badUser(){
+        alert("Name Doesn't Exist");
+    }
+</script>
+<?php
+    session_start();
+    $category = $_POST["category"];
+    $name = $_SESSION["name"];
 
-            $conn = new mysqli("127.0.0.1", "root", "", "archeryevent") or die("Connection failed : " . $conn->connect_error);
+    $conn = new mysqli("127.0.0.1", "root", "", "archeryevent") or die("Connection failed : " . $conn->connect_error);
 
-            // ! check if all non-null val is set
-            if(isset($category))
-            {
-                // check in db for existing name
-                $sql = "SELECT * FROM `user`
-                        WHERE `name` = '$name';";
+    // ! check if all non-null val is set
+    if(isset($category))
+    {
+        // check in db for existing name
+        $sql = "SELECT * FROM `user`
+                WHERE `name` = '$name';";
 
-                $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql);
 
-                // if result more than 0 , found user
-                // ? proceed to insert data
-                if ($result->num_rows > 0)
-                {
-                    $row = $result->fetch_assoc();
-                    $userID = $row["userID"];
+        // if result more than 0 , found user
+        // ? proceed to insert data
+        if ($result->num_rows > 0)
+        {
+            $row = $result->fetch_assoc();
+            $userID = $row["userID"];
 
-                    $sql = "INSERT INTO `event` (`userID`, `category`)
-                            VALUES ('$userID', '$category');";
+            $sql = "INSERT INTO `event` (`userID`, `category`)
+                    VALUES ('$userID', '$category');";
 
-                    mysqli_query($conn, $sql);
-                    
-                    header('Location: ../home_user.php');
-                    die();
-                }
-                // else if result is 0, did not found user
-                // ? return back to registerEvent
-                else
-                {
-                    header('Location: ../registerEvent.php');
-                    die();
-                }
-            }
-            // ! else one of the value above isnt set
-            // ? return back to registerEvent
-            else
-            {
-                header('Location: ../registerEvent.php');
-                die();
-            }
-
+            mysqli_query($conn, $sql);
             $conn->close();
-        ?>
-    </body>
-</html>
+            header('Location: ../home_user.php');
+            die();
+        }
+        // else if result is 0, did not found user
+        // ? return back to registerEvent
+        else
+        {
+            $conn->close();
+            echo "<script>badUser()</script>";
+            header('Location: ../registerEvent.php');
+            die();
+        }
+    }
+    // ! else one of the value above isnt set
+    // ? return back to registerEvent
+    else
+    {
+        header('Location: ../registerEvent.php');
+        die();
+    }
+?>
--- assets/registerProcess.php
+++ assets/registerProcess.php
@@ -1,54 +1,53 @@
-<?php session_start(); ?>
-<html>
-    <head>
-    </head>
-    <body>
-        <?php
-            $name = $_POST["name"];
-            $email = $_POST["email"];
-            $password = $_POST["password"];
-            $phoneNo = $_POST["phoneNo"];
+<script>
+    function badUser(){
+        alert("name already exist! Try to login.");
+    }
+</script>
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $phoneNo = $_POST["phoneNo"];
 
-            $conn = new mysqli("127.0.0.1", "root", "", "archeryevent") or die("Connection failed : " . $conn->connect_error);
+    include("DB_conn.php");
 
-            // ! check if all non-null val is set
-            if(isset($name) && isset($email) && isset($password))
-            {
-                // check in db for existing name
-                $sql = "SELECT `name` FROM `user`
-                        WHERE `name` = '$name';";
+    // ! check if all non-null val is set
+    if(isset($name) && isset($email) && isset($password))
+    {
+        // check in db for existing name
+        $sql = "SELECT `name` FROM `user`
+                WHERE `name` = '$name';";
 
-                $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql);
 
-                // if result return 0 , no conflict with existing users
-                // ? proceed to insert data
-                if ($result->num_rows == 0)
-                {
-                    $sql = "INSERT INTO `user` (`name`, `email`, `password`, `phoneNo`)
-                            VALUES ('$name', '$email', '$password', '$phoneNo');";
+        // if result return 0 , no conflict with existing users
+        // ? proceed to insert data
+        if ($result->num_rows == 0)
+        {
+            $sql = "INSERT INTO `user` (`name`, `email`, `password`, `phoneNo`)
+                    VALUES ('$name', '$email', '$password', '$phoneNo');";
 
-                    mysqli_query($conn, $sql);
-                    
-                    header('Location: ../login.php');
-                    die();
-                }
-                // else if result more than 0, conflict with existing users
-                // ? return back to register
-                else
-                {
-                    header('Location: ../register.php');
-                    die();
-                }
-            }
-            // ! else one of the value above isnt set
-            // ? return back to register
-            else
-            {
-                header('Location: ../register.php');
-                die();
-            }
+            mysqli_query($conn, $sql);
 
-            $conn->close();
-        ?>
-    </body>
-</html>
+            mysqli_close($conn);
+            header('Location: ../login.php');
+            die();
+        }
+        // else if result more than 0, conflict with existing users
+        // ? return back to register
+        else
+        {
+            mysqli_close($conn);
+            echo "<script>badUser()</script>";
+            header('Location: ../register.php');
+            die();
+        }
+    }
+    // ! else one of the value above isnt set
+    // ? return back to register
+    else
+    {
+        header('Location: ../register.php');
+        die();
+    }   
+?>
